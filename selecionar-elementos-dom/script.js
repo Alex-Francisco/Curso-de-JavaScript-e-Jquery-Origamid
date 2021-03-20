cf440d85c92a9656e955fb0dcb4efa30a3398969
Alex Francisco <alex2007f@gmail.com>
2021-03-20 10:51:23 -0300

    aprendendo jquery

---
+++ selecionar-elementos-dom/script.js
@@ -0,0 +1,9 @@
+$('.terceira').hide();
+$('a[href^="#').hide();
+
+var paragrafo = $('.paragrafo');
+console.log($('.paragrafo'));
+
+if($('span').length) {
+    console.log('Span existe na página');
+}
