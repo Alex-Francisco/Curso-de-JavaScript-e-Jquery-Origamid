6ae35ac42fb3e55f6bc3bca464ce25f2e968ddcc
Alex Francisco <alex2007f@gmail.com>
2021-03-20 20:26:47 -0300

    Update app.js

--- eventos-basicos/app.js
+++ eventos-basicos/app.js
@@ -1,3 +1,9 @@
-$('a').click(function(){
+/*$('a').click(function(){
+    $('.abrir-modal').text('Clicou');
+});*/
+
+//ou assim
+
+$('a').on('click', function() {
     $('.abrir-modal').text('Clicou');
 });
