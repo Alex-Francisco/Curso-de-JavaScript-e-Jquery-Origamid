c4aef003d32a456a636a9311b4877abd1f496c85
Alex Francisco <alex2007f@gmail.com>
2021-03-21 10:00:04 -0300

    Update app.js

--- estilos/app.js
+++ estilos/app.js
@@ -1,3 +1,4 @@
+/*
 var psize = $('p').css('font-size');
 console.log(psize);
 
@@ -8,3 +9,12 @@ $('.modal').css({
     'background-color': '#ccc',
     'width': '200px'
 });
+*/
+
+$(document).scroll(function() {
+    var documentTop = $(this).scrollTop();
+    $('.modal').css('height', documentTop * 5)
+})
+
+var width = $('.modal').width();
+console.log(width);
