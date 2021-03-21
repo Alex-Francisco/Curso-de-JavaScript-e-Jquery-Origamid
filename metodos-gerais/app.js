a52bd66cdd1fca17eff608a234cc2ff9443c3ca7
Alex Francisco <alex2007f@gmail.com>
2021-03-21 09:29:05 -0300

    praticando...

--- metodos-gerais/app.js
+++ metodos-gerais/app.js
@@ -1,2 +1,11 @@
 $('img').attr('alt', 'Imagem Castelos');
-$('img').removeAttr('src');
+$('img').removeAttr('alt');
+
+$('#botao').click(function() {
+    var nome = $('.nome').val();
+    $('.btn').text(nome);
+});
+
+$('.btn').click(function(){
+    $('input, #botao').remove();
+});
