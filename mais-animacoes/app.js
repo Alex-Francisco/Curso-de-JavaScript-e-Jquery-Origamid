4e0e610a53057302ef90b76287ac5c0d02d8612e
Alex Francisco <alex2007f@gmail.com>
2021-03-24 08:42:57 -0300

    animações

--- mais-animacoes/app.js
+++ mais-animacoes/app.js
@@ -1,3 +1,4 @@
+/*
 $('.btn').click(function(){
     $('.modal').fadeOut();
 });
@@ -5,3 +6,21 @@ $('.btn').click(function(){
 $('.abrir-modal').click(function(){
     $('.modal').fadeIn();
 });
+
+$('.btn').click(function(){
+    $('.modal').hide();
+});
+
+$('.abrir-modal').click(function(){
+    $('.modal').show();
+});
+*/
+
+$('.btn').click(function(){
+    $('.modal').slideUp();
+});
+
+$('.abrir-modal').click(function(){
+    $('.modal').slideDown();
+})
+
